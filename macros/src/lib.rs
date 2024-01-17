354a9ee21cff2d367f8daf18a6acca4a5ac3a67e
bcarlet <8906114+bcarlet@users.noreply.github.com>
2024-01-16 20:55:45 -0500

    Support tuple structs

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote, quote_spanned};
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident, Index};
 
 #[proc_macro_derive(Mangle)]
 pub fn derive_mangle(input: TokenStream) -> TokenStream {
@@ -11,8 +11,7 @@ pub fn derive_mangle(input: TokenStream) -> TokenStream {
     let name = &input.ident;
     let buffer = format_ident!("w");
 
-    let source_name = source_name(name);
-    let (code, statements) = mangle_data(&buffer, &input.data);
+    let statements = mangle_data(&buffer, name, &input.data);
 
     let expanded = quote! {
         impl Mangle for #name {
@@ -20,9 +19,7 @@ pub fn derive_mangle(input: TokenStream) -> TokenStream {
             where
                 W: ::std::fmt::Write,
             {
-                #buffer.write_str(concat!(#code, #source_name))?;
                 #statements
-                #buffer.write_char('E')
             }
         }
     };
@@ -30,32 +27,12 @@ pub fn derive_mangle(input: TokenStream) -> TokenStream {
     TokenStream::from(expanded)
 }
 
-/// Computes a [`<source-name>`] given an [`<identifier>`].
-///
-/// [`<identifier>`]:
-///     https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangle.identifier
-/// [`<source-name>`]:
-///     https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangle.source-name
-fn source_name(ident: &Ident) -> String {
-    let ident = ident.to_string();
-
-    format!("{}{}", ident.len(), ident)
-}
-
-/// Generates a sequence of statements which format the given data. Also returns
-/// the code indicating the kind of the data.
-///
-/// The statements emit the following productions:
-///
-/// - A `struct` is formatted as a sequence of [`<expression>`].
-/// - An `enum` is formatted as a single [`<number>`].
+/// Formats a `struct` or `enum` as an [`<expression>`].
 ///
 /// [`<expression>`]:
 ///     https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangle.expression
-/// [`<number>`]:
-///     https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangle.number
-fn mangle_data(buffer: &Ident, data: &Data) -> (&'static str, TokenStream2) {
-    match data {
+fn mangle_data(buffer: &Ident, name: &Ident, data: &Data) -> TokenStream2 {
+    let (code, statements) = match data {
         Data::Struct(data) => match &data.fields {
             Fields::Named(fields) => {
                 let calls = fields.named.iter().map(|field| {
@@ -63,9 +40,24 @@ fn mangle_data(buffer: &Ident, data: &Data) -> (&'static str, TokenStream2) {
                     let span = field.span();
 
                     quote_spanned! {span=>
-                        Mangle::mangle(&self.#name, #buffer)
+                        self.#name.mangle(#buffer)
                     }
                 });
+
+                let statements = quote! { #( #calls?; )* };
+
+                ("tl", statements)
+            }
+            Fields::Unnamed(fields) => {
+                let calls =
+                    fields.unnamed.iter().enumerate().map(|(i, field)| {
+                        let index = Index::from(i);
+                        let span = field.span();
+
+                        quote_spanned! {span=>
+                            self.#index.mangle(#buffer)
+                        }
+                    });
 
                 let statements = quote! { #( #calls?; )* };
 
@@ -81,5 +73,14 @@ fn mangle_data(buffer: &Ident, data: &Data) -> (&'static str, TokenStream2) {
             ("L", statements)
         }
         _ => unimplemented!(),
+    };
+
+    let identifier = name.to_string();
+    let prefix = format!("{}{}{}", code, identifier.len(), identifier);
+
+    quote! {
+        #buffer.write_str(#prefix)?;
+        #statements
+        #buffer.write_char('E')
     }
 }
